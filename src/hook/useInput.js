0c3b948b1c5de84f6d7d05ec7be9f1761b3d6ab5
jeongdopark <jdpark0816@gmail.com>
2023-04-07 18:35:38 +0900

    feat: useInput hook

---
+++ src/hook/useInput.js
@@ -0,0 +1,33 @@
+import React, { useState, useRef, useCallback } from 'react'
+
+// 유효성 검사
+
+export const useInput = (type) => {
+  const message = useRef({ value: '' })
+  const valid = useRef({ value: false })
+  const [inputValue, setInputValue] = useState('')
+
+  const checkValidation = useCallback(
+    (type) => (e) => {
+      setInputValue(e.target.value)
+      switch (type) {
+        case 'email':
+          valid.current = /@/.test(e.target.value)
+          if (!valid.current)
+            message.current = '이메일에 "@" 포함되어야 합니다.'
+          else message.current = ''
+          break
+        case 'password':
+          if (e.target.value.length >= 8) valid.current = true
+          else valid.current = false
+
+          if (!valid.current)
+            message.current = '비밀번호는 8자 이상이어야 합니다.'
+          else message.current = ''
+      }
+    },
+    [inputValue],
+  )
+
+  return [message, valid, checkValidation]
+}
