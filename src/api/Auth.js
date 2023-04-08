009b7576f4dca2ed4424a7e66fb0389142a5f18b
jeongdopark <jdpark0816@gmail.com>
2023-04-08 18:55:53 +0900

    feat: 회원가입, 로그인 api 연동

---
+++ src/api/Auth.js
@@ -0,0 +1,29 @@
+import { instance } from './client'
+
+export const signInApi = async (email, password) => {
+  try {
+    const response = await instance
+      .post('/auth/signin', { email, password })
+      .then((res) => {
+        console.log(res)
+        return res
+      })
+    return response
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const signUpApi = async (email, password) => {
+  try {
+    const response = await instance
+      .post('/auth/signup', { email, password })
+      .then((res) => {
+        console.log(res)
+        return res
+      })
+    return response
+  } catch (error) {
+    console.log(error)
+  }
+}
