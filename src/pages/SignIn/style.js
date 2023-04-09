46303918420a1d67835673c431a00e6e2483af38
jeongdopark <jdpark0816@gmail.com>
2023-04-09 21:07:56 +0900

    design: 회원가입, 로그인 페이지 디자인

--- src/pages/SignIn/style.js
+++ src/pages/SignIn/style.js
@@ -1,10 +1,14 @@
 import styled from 'styled-components'
 import { CommonContainer } from '../../style/commonContainer'
 
-const Container = styled(CommonContainer)``
+const Container = styled(CommonContainer)`
+  height: 400px;
+`
+
 const InputContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-around;
 `
 
 const ButtonContainer = styled.div``
