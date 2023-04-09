2134f3a5e3d3782ba33084d18f2ea931065621c5
jeongdopark <jdpark0816@gmail.com>
2023-04-09 21:00:55 +0900

    design: react-toastify 적용

--- src/pages/Todo/index.jsx
+++ src/pages/Todo/index.jsx
@@ -1,7 +1,9 @@
-import React, { createContext, useMemo, useState, useEffect } from 'react'
+import React, { createContext, useMemo } from 'react'
 import TodoInput from '../../components/Todo/TodoInput'
 import TodoList from '../../components/Todo/TodoList'
 import { useGetTodos } from '../../hook/useGetTodo'
+import { useNavigate } from 'react-router'
+import { toast } from 'react-toastify'
 import { S } from './style'
 
 export const TodoContext = createContext({
@@ -10,11 +12,20 @@ export const TodoContext = createContext({
 })
 
 const Todo = () => {
+  const navigate = useNavigate()
   const [todoList, getTodo] = useGetTodos()
-
+  const onLogOut = () => {
+    toast.success('로그아웃 완료')
+    localStorage.removeItem('token')
+    navigate('/')
+  }
   const value = useMemo(() => ({ todoList, getTodo }), [todoList, getTodo])
   return (
     <S.Container>
+      <S.Header>
+        <S.Title>Todo List</S.Title>
+        <S.LogoutButton onClick={onLogOut}>Logout</S.LogoutButton>
+      </S.Header>
       <TodoContext.Provider value={value}>
         <TodoInput />
         <TodoList />
